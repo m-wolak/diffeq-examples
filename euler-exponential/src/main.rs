3fcc00e379e58e807e445ffdb40e6ecd03f0afa2
Matt Wolak <mwolak@gmail.com>
2021-08-30 16:00:34 -0400

    add some comments

--- euler-exponential/src/main.rs
+++ euler-exponential/src/main.rs
@@ -3,18 +3,20 @@ use std::iter::successors;
 use macroquad::prelude::*;
 use macroquad::ui::*;
 
-// This is the function that matters to us: Euler's method for approximating a
-
+// This is the function that matters to us: Euler's method for approximating a differential equation
+// 
 fn forward_euler_step(last_x: f32, last_y: f32, f: fn(f32, f32) -> f32, delta: f32) -> (f32, f32) {
-    let next_y = last_y + delta * f(last_x, last_y);
-    let next_x = last_x + delta;
+    let next_y = last_y + delta * f(last_x, last_y); // new y is old y plus delta times the derivative
+    let next_x = last_x + delta;                    // new x is old x plus delta
     (next_x, next_y)
 }
 
 #[macroquad::main("Test-o-matic")]
 async fn main() {
-    let mut delta_inv: f32 = 10.0;
+    let mut delta_inv: f32 = 10.0; //default to delta_x = 1/10
 
+
+    // Set up the camera
     let w = screen_width();
     let h = screen_height();
     let mut ar = (w as f32) / (h as f32);
@@ -24,17 +26,17 @@ async fn main() {
         w: 3.0 * ar,
         h: 3.0,
     });
-
-    
-
-    let mut label_str = "".to_string();
     set_camera(&cam);
+    
+    // default label for e display
+    let mut label_str = "".to_string();
+    
     loop {
         
         let w = screen_width();
         let h = screen_height();
         
-        let new_ar = (w as f32) / (h as f32);
+        let new_ar = (w as f32) / (h as f32);  // check if we need to update the camera
         if new_ar != ar {
             let cam = Camera2D::from_display_rect(Rect {
                 x: -1.5 * ar,
@@ -47,11 +49,16 @@ async fn main() {
         }
         clear_background(WHITE);
 
+        // This is the starting point for the diffeq. It's upside down because
+        // macroquad has increasing y -> down
         let world_pt = vec2(0.0, -1.0);
 
+
+        // Draw the x and y axes
         draw_line(0.0, 0.0, 0.0, -10.0, 0.01, BLACK);
         draw_line(-10.0, 0.0, 10.0, 0.0, 0.01, BLACK);
 
+        // Draw in lines at x = 1 and y = e
         draw_line(1.0, 0.0, 1.0, -10.0, 0.01, GREEN);
         draw_line(
             -10.0,
@@ -62,28 +69,36 @@ async fn main() {
             GREEN,
         );
 
+        // set up the slider to change delta_x
         root_ui().window(321, vec2(10.0, 10.0), vec2(500.0, 60.0), |ui| {
-            ui.slider(123, "1/dx", 1.0..100.0, &mut delta_inv);
+            ui.slider(123, "1/delta_x", 1.0..100.0, &mut delta_inv);
             ui.label(None, &label_str)
         });
-        delta_inv = delta_inv.floor();
-        //root_ui().slider(123,"DX",0.0..0.5,&mut delta);
-        let delta = 1.0 / delta_inv;
+        delta_inv = delta_inv.floor(); // we want an integer number of steps per unit time
+        
+        let delta = 1.0 / delta_inv; // delta is delta_x
         let pt_size = delta.min(0.02);
         let w_x = world_pt.x;
         let w_y = world_pt.y;
-        draw_circle(w_x, w_y, 0.001, RED);
+        draw_circle(w_x, w_y, pt_size, RED); // draw a red dot at the starting time
+
+
+        // when to stop calculating points
         let max_x = cam
             .screen_to_world(Vec2::new(screen_width(), screen_height()))
             .x;
         
         let min_x = cam.screen_to_world(Vec2::ZERO).x;
+
+        // Here is where we calculate the diffeq using Euler's method, forward from (0,1)
         let forward_stream = successors(Some((w_x, w_y)), move |(x, y)| {
             Some(forward_euler_step(*x, *y, |_x, y| y, delta))
         });
         let forward_points: Vec<(f32, f32)> =
             forward_stream.take_while(|(x, _)| *x <= max_x).collect();
 
+
+        // draw in the forward points
         for (wx, wy) in forward_points {
             draw_circle(wx, wy, pt_size, BLUE);
             
@@ -91,17 +106,22 @@ async fn main() {
                 label_str = (-1.0 * wy).to_string();
             }
         }
+
+        // Calculate the diffeq backwards from (0,1). With Euler's method, it's as easy as picking a negative delta_x!
         let backwards_stream = successors(Some((w_x, w_y)), move |(x, y)| {
             Some(forward_euler_step(*x, *y, |_x, y| y, -1.0 * delta))
         });
         let backwards_points: Vec<(f32, f32)> =
             backwards_stream.take_while(|(x, _)| *x >= min_x).collect();
 
+        // draw in backward points
         for (wx, wy) in backwards_points {
             draw_circle(wx, wy, pt_size, BLUE);
             
         }
 
+
+        // This part just draws in the true graph of y = e^x
         let mut xval = min_x;
         let mut yval = -1.0*xval.exp();
         while xval < max_x {
